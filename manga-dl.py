7a58873aee9e53b122e89418b5b96003693263bf
Mu-Wei Chung <raymondc0302@gmail.com>
2022-12-18 11:15:03 +0800

    add alter method to dl for shuffled image

--- manga-dl.py
+++ manga-dl.py
@@ -1,29 +1,109 @@
 #!/usr/bin/env python3
 
+import base64
+import os
+import time
 import scrape
 from sys import argv
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 
 def main():
     if len(argv) < 2:
         print('Usage: python run.py [manga_url]')
         return
-    # Get the manga url from the command line
+    # Get the manga url from the second command line argument
     manga_url = argv[1]
-    # Create a new instance of the Chrome driver
-    driver = webdriver.Chrome()
-    # Load MangaReader.to page
-    driver.get(manga_url)
-    # Click the vertical button to select vertical reading mode
-    button = driver.find_element(By.XPATH, '//*[@id="first-read"]/div[1]/div/div[3]/a[1]')
-    # print(button.text)
-    button.click()
 
+    # webdriver settings
+    options = webdriver.ChromeOptions()
+    # options.add_argument('--headless') # Run Chrome in headless mode
+    options.add_experimental_option('prefs', {
+        "excludeSwitches": ["disable-popup-blocking"]
+    })
+    # Create a new instance of the Chrome driver
+    driver = webdriver.Chrome(options=options)
+    # Load MangaReader.to page
+    print("Loading MangaReader.to website...")
+    driver.get(manga_url)
+    
+    # Click the vertical button to select vertical reading mode
+    scrape.click_vertical_reading_mode_button(driver)
+    time.sleep(1)
+    window_name_ls = driver.window_handles
+    driver.switch_to.window(window_name_ls[0])  # switch to the first window (mangareader.to)
+
+    
+    
+    
+    ### ORIGINAL METHOD: CANNOT DEAL WITH SHUFFLED IMAGE ###
     # Get all the links to the manga page images
     links = scrape.get_all_manga_pages_links(driver.page_source)
-    # Download and save all the images
-    scrape.download_manga_pages(links, dirname= '_'.join(manga_url.split('/')[-3:]))
+    if links is not None:
+        print("Use original method to download the manga pages...")
+        # Download and save all the images
+        scrape.download_manga_pages(links, dirname= '_'.join(manga_url.split('/')[-3:]))
+    else:
+        print("Images were shuffled, use alternative method to download the manga pages...")
+        # Wait for the page to load
+        print("Loading manga page images...")
+        scrape.wait_for_image_to_load(driver)
+
+        # Get the base64 image
+        base64_image_ls = get_base64_image(driver)
+        print(f"Total pages found: {len(base64_image_ls)}")
+
+        # Save the images
+        print("Saving images...")
+        save_images(base64_image_ls, dirname= '_'.join(manga_url.split('/')[-3:]))
+        print("Done!")
+
+def get_base64_image(driver):
+    """
+    Get the base64 image of all the canvas tags where the manga page images are stored
+    """
+
+    # Get the base64 image of all the canvas tags using querySelectorAll and toDataURL
+    image_data_url_ls = driver.execute_script("""
+        const canvasList = document.querySelectorAll('canvas');
+        dataURLList = []
+        for (let i = 0; i < canvasList.length; i++) {
+            const canvas = canvasList[i];
+            const dataURL = canvas.toDataURL('image/png').substring(21);
+            dataURLList.push(dataURL);
+        }
+        return dataURLList;
+    """)
+
+    # Decode the base64 images
+    base64_image_ls = []
+    for image_data_url in image_data_url_ls:
+        base64_image_ls.append(base64.b64decode(image_data_url))
+    
+    return base64_image_ls
+
+def save_images(base64_image_ls, dirname=None):
+    """
+    Create a folder and save the base64 images png files
+    """
+    
+    # Create a new directory with the current date and time
+    if dirname is None:
+        dirname = time.strftime('%Y-%m-%d_%H-%M-%S')
+    
+    sequence_cnt = 1
+    # If the directory already exists, create a new one with a sequence number
+    while os.path.exists(dirname):
+        if sequence_cnt > 1:
+            dirname = dirname.rstrip('_' + str(sequence_cnt - 1))
+        dirname = dirname + '_' + str(sequence_cnt)
+    os.makedirs(dirname, exist_ok=False)
+    
+    # Save all the images
+    page_cnt = 1
+    for base64_image in base64_image_ls:
+        with open(dirname + '/page'+str(page_cnt) + '.png', 'wb') as f:
+            f.write(base64_image) # write the base64 image to a file
+        page_cnt += 1
 
 if __name__ == '__main__':
     main()
--- scrape.py
+++ scrape.py
@@ -1,4 +1,6 @@
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import os
 import time
@@ -7,16 +9,52 @@ def get_html(url):
     r = requests.get(url)
     return r.text
 
-def get_all_manga_pages_links(html):
+def click_vertical_reading_mode_button(driver):
+    # Get the vertical reading mode button
+    button = driver.find_element(By.XPATH, '//*[@id="first-read"]/div[1]/div/div[3]/a[1]')
+    # Click the button
+    button.click()
+
+def get_vertical_content_container(html):
+    """
+    Helper function for get_all_manga_pages_links.\n
+    Return the manga page container.
+    """
     soup = BeautifulSoup(html, 'html.parser')
     container = soup.find('div', id='vertical-content')
+    return container
+    
+def get_all_manga_pages_links(html):
+    container = get_vertical_content_container(html)
     pages = container.find_all('div')
     links = []
     for page in pages:
         link = page.get('data-url')
         if link is not None:
-            links.append(link)
+            if 'shuffled' in page['class']: 
+                return None
+            else:
+                links.append(link)
     return links
+
+def wait_for_image_to_load(driver):
+    time.sleep(1)   #TODO: Find a better way to wait for the page to load
+   
+    # Scroll down to view all image one by one to load all the images
+    total_height = int(driver.execute_script("return document.body.scrollHeight"))
+    # driver.execute_script("window.scrollTo(0, {});".format(total_height))
+    # print("initial total_height: {}".format(total_height))
+    current_height = 0
+    interval = 50
+    while current_height < total_height:
+        driver.execute_script("window.scrollTo(0, {});".format(current_height))
+        total_height = int(driver.execute_script("return document.body.scrollHeight"))
+        current_height += interval
+        time.sleep(0.01)
+        # if current_height % 1000 == 0:
+        #     print("{}-th iteration current_height: {}, total_height: {}".format(current_height//interval, current_height, total_height))
+    time.sleep(1)
+
 
 def download_manga_pages(links, dirname=None):
     # Create a new directory with the current date and time
