3aed96c5fb2304a0466a07da3b656b90b5f4d5cf
Mu-Wei Chung <raymondc0302@gmail.com>
2023-09-24 07:40:41 +1000

    add MANIFEST.in to include requirements.txt

--- setup.py
+++ setup.py
@@ -1,29 +1,26 @@
+import os
 from setuptools import setup, find_packages
 
-# Read the dependencies from requirements.txt
 
-with open('requirements.txt') as f:
-    dependencies = f.read().splitlines()
-
-with open('readme.md') as f:
-    long_description = f.read()
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='manga-dl',
     version='0.3.0',
     description='Download manga from mangareader.to website by the URL of the manga chapter/volume',
-    long_description=long_description,
+    long_description=read('README.md'),
     long_description_content_type='text/markdown',
     author='Mu-Wei Chung',
     author_email='raymondc0302@gmail.com',
     url='https://github.com/chungmuwei/MangaReaderDL',
-    packages=find_packages(),
+    packages=['app'],
     entry_points={
         'console_scripts': [
             'manga-dl=app.manga_dl:main',
         ],
     },
-    install_requires= dependencies,
+    install_requires=read('requirements.txt').splitlines(),
     classifiers=[
         'Development Status :: 4 - Beta',  
         'Intended Audience :: End Users/Desktop',
